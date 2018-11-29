b2deb0da64a910a5351c97254e56d2738f5c62cf
mathiasfc <mathias.falci@gmail.com>
2018-11-29 13:16:38 -0200

    fix width

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,29 @@
-var linhaMatriz = 7;
-var colunaMatriz = 7;
+/* Configuração inicial do tabuleiro*/
+let nrLinhaMatriz = 5;
+let nrColunaMatriz = 5;
+let rainhas = 5;
+
+/* Variáveis Globais */
 let solucoes = 0;
 
 window.onload = function() {
-  criaTabuleiro(linhaMatriz, colunaMatriz);
-
-  //addRainha(3, 3);
-  let rainhas = 8;
+  criaTabuleiro(nrLinhaMatriz, nrColunaMatriz);
   let colunaAtual = 0;
   backtracking(rainhas, colunaAtual);
 };
 
-let criaTabuleiro = (linhaMatriz, colunaMatriz) => {
+let criaTabuleiro = (nrLinhaMatriz, nrColunaMatriz) => {
+  let inverteCor = false;
   let matriz = $('.matriz');
-  for (let i = 0; i < linhaMatriz; i++) {
+  for (let i = 0; i < nrLinhaMatriz; i++) {
     matriz.append("<div class='linha'>");
-    for (let j = 0; j < colunaMatriz; j++) {
-      let bloco = "<div class='bloco' linha=" + i + ' coluna=' + j + '></div>';
+    for (let j = 0; j < nrColunaMatriz; j++) {
+      let bloco = "<div class='bloco " + (inverteCor ? 'black' : 'white') + "' linha=" + i + ' coluna=' + j + '></div>';
+      inverteCor = !inverteCor;
       $('.linha')
         .last()
         .append(bloco);
-      if (j == colunaMatriz) matriz.append('</div>');
+      if (j == nrColunaMatriz) matriz.append('</div>');
     }
   }
 };
@@ -38,14 +41,15 @@ let removeRainha = (li, col) => {
 	retorna true se conseguiu resolver e false caso contrário
 */
 let backtracking = (rainhas, col) => {
-  if (solucoes > 10) {
+  /*if (solucoes > 1) {
     return;
-  }
+  }*/
   if (col == rainhas) {
     let matrizSolucao = $('.matriz[type="default"]').clone();
     matrizSolucao.attr('type', 'solution' + (solucoes + 1));
     matrizSolucao.removeClass('hide');
-    $('body').append(matrizSolucao);
+    $('.resultados').append(matrizSolucao);
+    $('div[type="solution' + (solucoes + 1)).before('<span class="span-solucao">Solução ' + (solucoes + 1) + '</span>');
     console.log('\nSolucao ' + (solucoes + 1) + ' encontrada.');
     solucoes++;
     return;
@@ -53,18 +57,12 @@ let backtracking = (rainhas, col) => {
   for (let i = 0; i < rainhas; i++) {
     // verifica se é seguro colocar a rainha naquela coluna
     if (posicaoValida(rainhas, i, col)) {
-      // insere a rainha (marca com 1)
       addRainha(i, col);
-      //tab[i][col] = 1;
-      //$('.bloco[linha=' + i + '][coluna=' + col + ']').addClass('rainha');
 
       // chamada recursiva
       backtracking(rainhas, col + 1);
 
-      // remove a rainha (backtracking)
       removeRainha(i, col);
-      //$('.bloco[linha=' + i + '][coluna=' + col + ']').removeClass('rainha');
-      //tab[i][col] = 0;
     }
   }
 };
@@ -75,112 +73,32 @@ let posicaoValida = (rainhas, lin, col) => {
 
   // verifica se ocorre ataque na linha
   for (i = 0; i < rainhas; i++) {
-    //if (tab[lin][i] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + lin + '][coluna=' + i + ']').hasClass('rainha')) return false;
   }
 
   //verifica se ocorre ataque na coluna
   for (i = 0; i < rainhas; i++) {
-    //if (tab[i][col] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + i + '][coluna=' + col + ']').hasClass('rainha')) return false;
   }
 
   // verifica se ocorre ataque na diagonal principal
   // acima e abaixo
   for (i = lin, j = col; i >= 0 && j >= 0; i--, j--) {
-    //if (tab[i][j] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + i + '][coluna=' + j + ']').hasClass('rainha')) return false;
   }
   for (i = lin, j = col; i < rainhas && j < rainhas; i++, j++) {
-    //if (tab[i][j] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + i + '][coluna=' + j + ']').hasClass('rainha')) return false;
   }
 
   // verifica se ocorre ataque na diagonal secundária
   // acima e abaixo
   for (i = lin, j = col; i >= 0 && j < rainhas; i--, j++) {
-    //if (tab[i][j] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + i + '][coluna=' + j + ']').hasClass('rainha')) return false;
   }
   for (i = lin, j = col; i < rainhas && j >= 0; i++, j--) {
-    //if (tab[i][j] == 1) return false;
     if ($('.matriz[type="default"] .bloco[linha=' + i + '][coluna=' + j + ']').hasClass('rainha')) return false;
   }
 
   // se chegou aqui, então está seguro (retorna true)
   return true;
 };
-
-function marcaPosicoes(li, col) {
-  /*mesma coluna ou mesma linha faz parte range
-
-    se -1 linha e -1 coluna faz parte
-
-    se +1 linha e +1 coluna faz parte
-
-    se +1 linha e -1 coluna faz parte
-
-    se -1 linha e +1 coluna faz parte*/
-  for (var i = 0; i < linhaMatriz; i++) {
-    for (var j = 0; j < colunaMatriz; j++) {
-      if (!$('.bloco[linha=' + i + '][coluna=' + j + ']').hasClass('rainha')) {
-        if (li - 1 == i && col - 1 == j) {
-          $('.bloco[linha=' + i + '][coluna=' + j + ']').addClass('rainha');
-          marcaPosicoes(i, j);
-        } else if (li + 1 == i && col + 1 == j) {
-          $('.bloco[linha=' + i + '][coluna=' + j + ']').addClass('rainha');
-          marcaPosicoes(i, j);
-        } else if (li + 1 == i && col - 1 == j) {
-          $('.bloco[linha=' + i + '][coluna=' + j + ']').addClass('rainha');
-          marcaPosicoes(i, j);
-        } else if (li - 1 == i && col + 1 == j) {
-          //$(".bloco[linha="+i+"][coluna="+j+"]").addClass('rainha');
-          //marcaPosicoes(i,j);
-        }
-      }
-    }
-  }
-
-  /*//cima
-    for(var i=col;i>=0;i--){
-        $(".bloco[linha="+i+"][coluna="+li+"]").addClass('rainha');
-    }
-
-    //baixo
-    for(var i=col;i<=colunaMatriz;i++){
-        $(".bloco[linha="+i+"][coluna="+li+"]").addClass('rainha');
-    }
-
-    //esquerda
-    for(var i=col;i>=0;i--){
-        $(".bloco[linha="+li+"][coluna="+i+"]").addClass('rainha');
-    }
-
-    //direita
-    for(var i=col;i<=colunaMatriz;i++){
-        $(".bloco[linha="+li+"][coluna="+i+"]").addClass('rainha');
-    }
-
-    //diagonais esquerda-cima
-    for(var i=li;i>=0;i--){
-        $(".bloco[linha="+i+"][coluna="+i+"]").addClass('rainha');
-    }
-
-    //diagonais direita-baixo
-    for(var i=li;i<=linhaMatriz;i++){
-        $(".bloco[linha="+i+"][coluna="+i+"]").addClass('rainha');
-    }
-
-
-    //todo
-
-    //diagonais direita-cima
-    for(var i=li;i<=linhaMatriz;i++){
-        $(".bloco[linha="+i+"][coluna="+i+"]").addClass('rainha');
-    }
-
-    //diagonais esquerda-baixo
-    for(var i=li;i<=linhaMatriz;i++){
-        $(".bloco[linha="+i+"][coluna="+i+"]").addClass('rainha');
-    }*/
-}
